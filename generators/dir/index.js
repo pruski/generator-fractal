64aac1977f9f26b96a8da15f8c4651884418c20d
pruski <konrad.pruski@gmail.com>
2015-06-16 22:02:36 +0100

    adding more flexibility to directive generator

--- generators/dir/index.js
+++ generators/dir/index.js
@@ -6,10 +6,64 @@ module.exports = subgenFactory.create({
     _context: 'js',
     _filenamePostfix: '-directive.js',
 
+    prompting: function () {
+        var done = this.async();
+
+        var prompts = [{
+            type: 'list',
+            choices: ['yes', 'no'],
+            name: 'link',
+            message: 'Do you need link function?',
+            default: 'no'
+        }];
+
+        if(!this.options.deep && !this._isSubgenQueued('ctrl')) {
+            prompts.push({
+                type: 'list',
+                choices: ['yes', 'no'],
+                name: 'ctrl',
+                message: 'Do you need a Controller?',
+                default: 'no'
+            });
+        }
+
+        if(!this.options.deep && !this._isSubgenQueued('tpl')) {
+            prompts.push({
+                type: 'list',
+                choices: ['yes', 'no'],
+                name: 'tpl',
+                message: 'Do you need a Template?',
+                default: 'no'
+            });
+        }
+
+        this.prompt(prompts, function (props) {
+            if(props.link === 'yes') {
+                this.options.linkFn = true;
+
+                if(this._fractalConfig.unitTests) {
+                    this._scheduleSubgen('tdir');
+                }
+            }
+
+            if(props.ctrl === 'yes') {
+                this._scheduleSubgen('ctrl');
+            }
+
+            if(props.tpl === 'yes') {
+                this._scheduleSubgen('tpl');
+            }
+
+            this.log('---------------------------------');
+            done();
+        }.bind(this));
+    },
+
     _getExtraTemplateVars: function() {
         return {
-            ctrl             : this._subgenerators.indexOf('ctrl') > -1,
-            tpl              : this._subgenerators.indexOf('tpl') > -1,
+            link             : this.options.linkFn,
+            ctrl             : this._isSubgenQueued('ctrl'),
+            tpl              : this._isSubgenQueued('tpl'),
             templateCacheName: (this._fractalCtxFound ? this._fractalConfig.tplDir : "") + this._fractalCtxRelativePath + this._rawFilePath + '.html',
         };
     }
